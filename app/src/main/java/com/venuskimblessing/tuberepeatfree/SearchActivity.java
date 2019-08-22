37b76654207bda3cc01e28649ba56df458e74085
venus <venuskimblessing@gmail.com>
2019-08-22 14:16:08 +0900

    마스터 브런치 개발

--- app/src/main/java/com/venuskimblessing/tuberepeatfree/SearchActivity.java
+++ app/src/main/java/com/venuskimblessing/tuberepeatfree/SearchActivity.java
@@ -187,6 +187,8 @@ public class SearchActivity extends BaseActivity implements SearchRecyclerViewAd
         startPlayYouTubeShare(getIntent());
         Log.d(TAG, "테스트1212133332");
         Toast.makeText(this, "테스트 마스터 브런치111", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "마스터 브런치 개발");
+
     }
 
     private void initView() {
