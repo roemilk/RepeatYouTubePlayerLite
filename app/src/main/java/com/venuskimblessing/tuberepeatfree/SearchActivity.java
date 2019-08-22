0c8bf439f9e1001d165060ef50985a102c145be4
venus <venuskimblessing@gmail.com>
2019-08-22 12:51:34 +0900

    test11212

--- app/src/main/java/com/venuskimblessing/tuberepeatfree/SearchActivity.java
+++ app/src/main/java/com/venuskimblessing/tuberepeatfree/SearchActivity.java
@@ -185,7 +185,7 @@ public class SearchActivity extends BaseActivity implements SearchRecyclerViewAd
         initRetrofit();
         loadPopularContentsList();
         startPlayYouTubeShare(getIntent());
-        Log.d(TAG, "테스트");
+        Log.d(TAG, "테스트121212");
     }
 
     private void initView() {
